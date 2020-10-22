a90b6c7a11e5114f1624c4c9bc787ba69005c641
Dawid Napora <dawnap24@gmail.com>
2020-10-22 22:49:11 +0200

    Add Composition API

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 import './registerServiceWorker'
+
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+
+import vuetify from './plugins/vuetify'
+import './plugins/composition-api'
 
 Vue.config.productionTip = false
 
